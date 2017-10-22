1f3a9a2c7f4701c856afd64b9e303773b258bea6
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-10-21 21:19:17 -0500

    working through Reporting a Sighting submit form. Not receiving info to db.

--- routes/reportSight.js
+++ routes/reportSight.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 //var Housing = require('./housing');
 
 //connect to DB
-mongoose.connect('localhost:27017/realestate');
+mongoose.connect('localhost:27017/watchale');
 
 //POST function
 app.post('/reporting', (req, res) => {
@@ -13,7 +13,7 @@ mondoDB.collection('reporting').save(req.body, (err, result) => {
     if (err) return console.log(err)
 
     console.log('saved sighting report to database')
-    res.redirect('/sightings')
+    res.redirect('#!/sightings')
     })
 })
 
