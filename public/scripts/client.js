20fa071665a9ee3b58c8c28234d892856f48737d
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-10-26 18:07:36 -0500

    added some style to it. icons were added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,14 +3,14 @@ var myApp = angular.module('myApp', ['ngMaterial', 'ngRoute']);
 
 /// Routes ///
 // googleAuthApp.config(['$routeProvider', function ($routeProvider) {
-myApp.config(function ($routeProvider) {
- //NEED TO FIND A FUNCTION THAT ONLY ALLOWS YOU TO SEE PAGES IF YOU'RE LOGGED-IN
+myApp.config(function ($routeProvider, $mdThemingProvider) {
+  //NEED TO FIND A FUNCTION THAT ONLY ALLOWS YOU TO SEE PAGES IF YOU'RE LOGGED-IN
   $routeProvider
     .when('/', {
       templateUrl: 'views/landing.html',
       controller: 'LandingController as dc'
-    })  
-  .when('/login', {
+    })
+    .when('/login', {
       templateUrl: 'views/login.html',
       controller: 'LoginController as lc'
     })
@@ -33,4 +33,20 @@ myApp.config(function ($routeProvider) {
     .otherwise({
       redirectTo: '/',
     });
+  // $mdThemingProvider.theme('default')
+  //   .primaryPalette('pink', {
+  //     'default': '400', // by default use shade 400 from the pink palette for primary intentions
+  //     'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+  //     'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+  //     'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+  //   })
+  //   .accentPalette('Cyan');
+
+  $mdThemingProvider.theme('default')
+  .primaryPalette('light-blue', {
+    'default': '700'})
+    .accentPalette('pink', {
+    'default': 'A200'
+
+  });
 });
--- public/scripts/services/app.service.js
+++ public/scripts/services/app.service.js
@@ -38,6 +38,4 @@ myApp.service('AppService', function ($http) {
         })
     }
 
-
-
 }); //end myApp.service
